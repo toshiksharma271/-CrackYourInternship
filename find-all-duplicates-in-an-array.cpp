c7e9aabff2787b65547df548dcd94c50e75b8778
baingan <98310467+toshiksharma271@users.noreply.github.com>
2024-07-19 17:45:01 +0530

    Question 11 - find-all-duplicates-in-an-array

--- find-all-duplicates-in-an-array.cpp
+++ find-all-duplicates-in-an-array.cpp
@@ -1,3 +1,5 @@
+$ brute
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
@@ -14,3 +16,27 @@ public:
         
     }
 };
+
+$ optimal
+
+#define pb push_back
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>v;
+        for(int i=0;i<n;i++){
+           int num = abs(nums[i]);
+           int index = num-1;
+           if(nums[index]<0){
+            v.pb(num);
+           }
+           else{
+            nums[index]*=-1;
+           }
+        }
+        return v;
+
+    }
+};
+
